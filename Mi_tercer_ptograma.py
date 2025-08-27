534c521854578e039ab8f15a96328fa796609569
LauraSofia9 <larchil61276@universidadean.edu.co>
2025-08-27 16:08:22 -0500

    Add files via upload

---
+++ Mi_tercer_ptograma.py
@@ -0,0 +1,25 @@
+#Mi_tercer_ptograma
+
+print ("Bienvenido a el mundo phyton")
+
+
+nombre="Luna"
+#edad="130"
+altura="2.01"
+es_estudiante=True
+
+usuario=input("escriba su nombre    ")
+print ("hola señor usuario  " ,usuario)
+
+suma= 45+52
+print ("El resultado de la suma de 45 + 52 es " , suma)
+
+#vamos a hacer una estructura de control (ciclo)
+
+edad= int(input("Escribe tú edad") )
+
+if edad >= 18:
+    print("eres mayor de edad ")
+else: 
+    print("eres menor de edad")
+    
